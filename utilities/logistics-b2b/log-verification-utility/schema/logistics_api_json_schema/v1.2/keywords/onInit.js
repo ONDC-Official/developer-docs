87fe16e9f8ac60883fa6e5f4c06bb6c968a6836f
Abhinav Goyal <abhinavgoyal245@gmail.com>
2023-12-28 11:55:34 +0530

    minor change

--- utilities/logistics-b2b/log-verification-utility/schema/logistics_api_json_schema/v1.2/keywords/onInit.js
+++ utilities/logistics-b2b/log-verification-utility/schema/logistics_api_json_schema/v1.2/keywords/onInit.js
@@ -1,6 +1,6 @@
 module.exports = {
   isQuoteMatching: (data) => {
-    const quotePrice = parseFloat(data?.price?.value);
+    let quotePrice = parseFloat(data?.price?.value);
     const breakupArr = data.breakup;
     let totalBreakup = 0;
     breakupArr.forEach((breakup) => {
