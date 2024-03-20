1df0475d72bbd8ad66b8ce5811e85ec50422becd
Abhinav Goyal <abhinavgoyal245@gmail.com>
2024-03-20 12:38:46 +0530

    removed services from main

--- utilities/logistics-b2b/log-verification-utility/schema/SRV_json_schema/keywords/init.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  isLengthValid: (data) => {
-    if (data?.name?.length + data?.building?.length + data?.locality?.length > 190)
-      return false;
-    else return true;
-  },
-};
--- utilities/logistics-b2b/log-verification-utility/schema/SRV_json_schema/keywords/search.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  isEndTimeGreater: (data) => {
-    const startTime = parseInt(data?.start);
-    const endTime = parseInt(data?.end);
-    return startTime < endTime;
-  },
-};
