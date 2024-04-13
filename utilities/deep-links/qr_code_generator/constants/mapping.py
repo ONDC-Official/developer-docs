a68eb98b14dc397017d73c38de7e57a10d54f5df
Tanya Madaan <tanyamadaan@Tanyas-MacBook-Air.local>
2024-04-13 17:58:48 +0530

    QR code generation utility updated

---
+++ utilities/deep-links/qr_code_generator/constants/mapping.py
@@ -0,0 +1,10 @@
+KEY_MAPPING = {
+    "bpp_id": "context.bpp_id",
+    "domain": "context.domain",
+    "provider_id": "message.intent.provider.id"
+}
+BASE_URL = "beckn://ondc"
+
+DEFAULT_PARAMS = {
+    "context.action": "search"
+}
--- utilities/deep-links/qr_code_generator/core.py
+++ utilities/deep-links/qr_code_generator/core.py
@@ -1,28 +1,35 @@
 import qrcode
 from PIL import Image
-import json
-import sys
 import os
+import csv
+from qr_code_generator.constants import mapping
 
-BASE_URL = "beckn://ondc"
 
-DEFAULT_PARAMS = {
-    "context.action": "search"
-}
 
+
+def bulk_read(data_file, qr_data={}, qr_list = []):
+    with open(data_file, mode ='r', encoding='utf-8-sig') as file: 
+        csvFile = csv.DictReader(file)
+        for row in csvFile:
+            data = dict(row)
+            for key, value in data.items():
+                k = mapping.KEY_MAPPING[key]
+                qr_data[k] = value
+            qr_list.append(qr_data)
+    return qr_list
 
 #  Generate deep link
-def generate_deep_link(**kwargs):
-    params = DEFAULT_PARAMS.copy()
-    params.update(kwargs)
+def generate_deep_link(deep_link):
+    params = mapping.DEFAULT_PARAMS.copy()
+    params.update(deep_link)
     query_string = '&'.join([f"{k}={v}" for k, v in params.items()])
     # fallback_url = "https://google.com"
-    print(f"{BASE_URL}?{query_string}")
-    return f"{BASE_URL}?{query_string}"
+    print(f"{mapping.BASE_URL}?{query_string}")
+    return f"{mapping.BASE_URL}?{query_string}"
 
 #  Generate QR code with deep link embedded
 
-def generate_qr_code(deep_link, file_name="qr_code.png"):
+def generate_qr_code(deep_link, output):
     """
     Generate a QR code from a given deep link.
 
@@ -34,15 +41,14 @@ def generate_qr_code(deep_link, file_name="qr_code.png"):
     - None
     """
     try:
-
         # load ONDC logo
-        logo_path = os.path.join(os.path.dirname(__file__), "logo.png")
+        logo_path = os.path.join(os.path.dirname(__file__), "assets/ondc-network-vertical.png")
 
         # Create QR code with deep link embedded
         qr = qrcode.QRCode(
             # version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=10,
+            box_size=8,
             border=4,
         )
         qr.add_data(deep_link)
@@ -56,7 +62,7 @@ def generate_qr_code(deep_link, file_name="qr_code.png"):
         logo = logo.convert("RGBA")
 
         # taking base width
-        basewidth = 400
+        basewidth = 150
 
         # adjust image size
         wpercent = (basewidth/float(logo.size[0]))
@@ -70,7 +76,7 @@ def generate_qr_code(deep_link, file_name="qr_code.png"):
         qr_img.paste(logo, logo_position, logo)
 
 
-        qr_img.save(file_name)
+        qr_img.save(output)
 
     except FileNotFoundError:
         print(f"Logo file not found at: {logo_path}")
